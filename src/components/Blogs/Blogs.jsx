44f357a35e6e85f738aee6b5c344141db3e66695
ASHRAFALI <111032888+ASHRAFALI-M@users.noreply.github.com>
2024-07-02 12:27:27 +0530

    Add files via upload

---
+++ src/components/Blogs/Blogs.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Blogs = () => {
+  return (
+    <section className="blogs-section">
+      <h2>BLOGS</h2>
+      <p>Check out my latest blog posts:</p>
+      <a 
+        href="https://yourblogurl.com" 
+        target="_blank" 
+        rel="noopener noreferrer"
+        className="blog-link"
+      >
+        Visit My Blog
+      </a>
+    </section>
+  );
+};
+
+export default Blogs;
